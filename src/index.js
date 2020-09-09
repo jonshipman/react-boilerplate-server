89ca0e951715102a591b2010180c2c3b5a3ddf93
Jon Shipman <jon@jonshipman.com>
2020-09-09 13:05:45 -0500

    move compatibility layer into exported function

--- src/index.js
+++ src/index.js
@@ -1,44 +1,48 @@
 import babel from "@babel/register";
 
-babel({
-  presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [
-    [
-      "babel-plugin-inline-import-data-uri",
-      { extensions: [".webp", ".jpg", ".gif", ".png"] },
-    ],
-    "babel-plugin-inline-react-svg",
-    "@babel/plugin-proposal-class-properties",
-  ],
-  ignore: ["*.scss", "*.css"],
-});
-
 // Provide jsdom replacements for browser properties
 import { JSDOM } from "jsdom";
-global.dom = new JSDOM(null, { url: "http://localhost" });
-global.window = global.dom.window;
-global.document = global.window.document;
-global.navigator = global.window.navigator;
 
 // Create mock storage (won't be used on the server, keeps the code from dying)
 import Storage from "dom-storage";
-global.localStorage = new Storage(null, { strict: true });
-global.sessionStorage = new Storage(null, { strict: true });
 
-global.matchMedia =
-  global.matchMedia ||
-  function () {
-    return {
-      matches: false,
-      addListener: function () {},
-      removeListener: function () {},
-    };
-  };
-
-// Ignore styles, provide a replacemen for fetch, and build upon canvas
+// Provide a replacemen for fetch, and build upon canvas
 import "isomorphic-fetch";
 import "canvas";
 
 import { server } from "./server";
 
-export { server };
+const init = () => {
+  babel({
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+    plugins: [
+      [
+        "babel-plugin-inline-import-data-uri",
+        { extensions: [".webp", ".jpg", ".gif", ".png"] },
+      ],
+      "babel-plugin-inline-react-svg",
+      "@babel/plugin-proposal-class-properties",
+    ],
+    ignore: ["*.scss", "*.css"],
+  });
+
+  global.dom = new JSDOM(null, { url: "http://localhost" });
+  global.window = global.dom.window;
+  global.document = global.window.document;
+  global.navigator = global.window.navigator;
+
+  global.localStorage = new Storage(null, { strict: true });
+  global.sessionStorage = new Storage(null, { strict: true });
+
+  global.matchMedia =
+    global.matchMedia ||
+    function () {
+      return {
+        matches: false,
+        addListener: function () {},
+        removeListener: function () {},
+      };
+    };
+};
+
+export { server, init };
