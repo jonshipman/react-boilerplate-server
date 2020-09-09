b0b83475ca50df4b1ff943815c5ba6df93bf5abe
Jon Shipman <jon@jonshipman.com>
2020-09-09 10:33:38 -0500

    corejs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   ignore: [/(node_modules)/],
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    ["@babel/preset-env", { useBuiltIns: "usage", corejs: 3 }],
+    "@babel/preset-react",
+  ],
   plugins: [
     [
       "babel-plugin-inline-import-data-uri",
